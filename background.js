9d46d1f2f68b019669ab00786cebd41cc69d9d42
betsy <8404974+betsy@users.noreply.github.com>
2019-11-16 13:51:19 -0500

    changes bg color

--- background.js
+++ background.js
@@ -16,4 +16,12 @@ chrome.runtime.onInstalled.addListener(function() {
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
+  chrome.runtime.onMessage.addListener(
+    function(message, callback) {
+      if (message == “changeColor”){
+        chrome.tabs.executeScript({
+          code: 'document.body.style.backgroundColor="orange"'
+        });
+      }
+   });
 });
